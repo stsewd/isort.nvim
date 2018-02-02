a873f517d73064b89e1c34eeafd40a06e2b1fb32
Santos Gallegos <santos_g@outlook.com>
2018-02-02 00:39:34 -0500

    Add plugin

---
+++ rplugin/python3/isort-nvim.py
@@ -0,0 +1,31 @@
+import neovim
+from subprocess import Popen, PIPE
+
+
+ISORT_COMMAND = 'isort'
+
+
+@neovim.plugin
+class IsortNvim:
+
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @neovim.command('Isort', nargs='*', range='%', complete='file')
+    def isort_command(self, args, range):
+        current_file = self.nvim.call('expand', '%')
+        if not current_file:
+            self.error('The current buffer is not saved')
+            return
+        output = self._isort(current_file)
+        lines = output.split('\n')[:-1]
+        self.nvim.current.buffer[:] = lines
+
+    def error(self, msg):
+        self.nvim.err_write('[isort] {}\n'.format(msg))
+
+    def _isort(self, file):
+        isort_command = self.nvim.vars.get('isort_command', ISORT_COMMAND)
+        args = [isort_command, '--stdout', file]
+        with Popen(args, stdout=PIPE) as proc:
+            return proc.stdout.read().decode()
